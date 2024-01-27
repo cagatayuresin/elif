bc5dc3d05e8545e5a88e9b247e271d8a017cf944
Çagatay Üresin <cagatay.uresin@detaysoft.com>
2024-01-27 11:24:19 +0300

    Fİle model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class FileType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    icon = models.ImageField(upload_to='filetype_icons/')
+
+    def __str__(self):
+        return self.name
+
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='uploads/')
+    file_type = models.ForeignKey(FileType,  on_delete=models.SET_DEFAULT, default=1)
+
+    def __str__(self):
+        return self.name
+
